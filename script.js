0799a1f37304dcd424c1162b13d870a517925fbc
dharmik264 <113712264+dharmik264@users.noreply.github.com>
2024-01-07 16:58:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const slider = document.querySelector('.slider');
 
 function activate(e) {
   const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0]);
-  e.target.matches('.prev') && slider.append(items[items.ngth -1]);
+
+  if (e.target.matches('.next')) {
+    // Move the first item to the end
+    slider.append(items[0].cloneNode(true));
+    items[0].remove();
+  }
+
+  if (e.target.matches('.prev')) {
+    // Move the last item to the beginning
+    slider.prepend(items[items.length - 1].cloneNode(true));
+    items[items.length - 1].remove();
+  }
 }
-document.addEventListener('click',activate,false);
+
+document.addEventListener('click', activate, false);
